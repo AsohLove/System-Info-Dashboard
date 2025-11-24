7011b2d7160988c6c61ba6834208d3596be8d2f8
Love Asoh <loveasoh@gmail.com>
2025-11-24 15:38:47 +0100

    Save output to a log file

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-echo "  ===== SYSTEM INFO =====    "
+
 
 current_user=$(whoami)
 host_name=$(hostname)
@@ -15,11 +15,32 @@ total_disk=$(df -h / | tail -1 | awk '{print $2}')
 used_disk=$(df -h / | tail -1 | awk '{print $3}')
 free_disk=$(df -h / | tail -1 | awk '{print $4}')
 
-process=$(ps aux | sort -nk 4 | head -5)
+total_run_proc=$(ps aux --no-heading| wc -l)
+top_five_proc=$(ps aux --sort -%mem | head -6 | awk '{print $1, $2, $3, $4, $5}')
 
 
-#cpu_mod=$(grep "model name" /proc/cpuinfo | head -1 | awk -F: '{print $4}')
+#Save the output to a log file
+{
+    echo "  ===== SYSTEM INFO =====    "
+    echo "User: $current_user Hostname: $host_name "
+    echo "Date: $current_date "
 
+    echo "   --- Uptime ---- "
+    echo "Uptime: $time_up "
+
+    echo "   ----- Memory (MB) ----- "
+    echo "Total: $total_memory | Used: $used_memory | Free: $free_memory "
+
+    echo " ----- Disk Usage----- " 
+    echo "Total: $total_disk | Used: $used_disk | Free: $free_disk "
+
+
+    echo " ----- Processes Running: ----- "
+    echo -e "Total current running processes: $total_run_proc Top five running processes: \n$top_five_proc "
+} > OUTPUT_FILE
+
+# Display the output on the terminal
+echo "  ===== SYSTEM INFO =====    "
 echo "User: $current_user Hostname: $host_name "
 echo "Date: $current_date "
 
@@ -34,6 +55,4 @@ echo "Total: $total_disk | Used: $used_disk | Free: $free_disk "
 
 
 echo " ----- Processes Running: ----- "
-echo -e "Top five running processes: \n$process"
-
-
+echo -e "Total current running processes: $total_run_proc Top five running processes: \n$top_five_proc"
